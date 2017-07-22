ae99c6959f0120bfc06da49eceda4f821edc7b20
Michael Baird <mike@baird.rocks>
2017-07-21 23:54:32 -0500

    Update AssemblyInfo.cs

--- src/Fluky/Properties/AssemblyInfo.cs
+++ src/Fluky/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
