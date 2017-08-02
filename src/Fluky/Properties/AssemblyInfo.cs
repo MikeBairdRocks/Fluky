469763d9d5ba60a4dd78f4e361c649dced96ce52
Michael Baird <mike@baird.rocks>
2017-08-01 20:54:14 -0500

    updated project icon

--- src/Fluky/Properties/AssemblyInfo.cs
+++ src/Fluky/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Emphatic Fist")]
+[assembly: AssemblyCompany("Fluky")]
 [assembly: AssemblyProduct("Fluky")]
 [assembly: AssemblyTrademark("")]
 
